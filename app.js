5b0e483b090d543440005030fd0bc9cd792d9902
Osmar <demi.gody666@gmail.com>
2021-02-24 21:51:03 -0400

    first commit

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/service', service);
+const port = 8000;
+app.listen(port, () =>{
+console.log("Corriendo en el puerto " + port)
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
